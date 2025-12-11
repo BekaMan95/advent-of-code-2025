44db90fd283bf70f1f08658eee16e16a65a08b00
Bereket Sahle <bsahle95@gmail.com>
2025-12-11 11:25:59 +0300

    Day 11 Solutions part two

--- solutions/day-11-part-2.py
+++ solutions/day-11-part-2.py
@@ -1,23 +1,27 @@
 
 
-def dfs(node: str):
+def dfs(node: str, dac_found: bool, fft_found: bool):
     if node in visited:
         return 0
     
     if node == "out":
-        return 1 if "dac" in visited and "fft" in visited else 0
+        return 1 if dac_found and fft_found else 0
     
-    res = 0
-    visited.add(node)
+    if (node, dac_found, fft_found) not in cache:
+        res = 0
+        dac_found = dac_found or (node == "dac")
+        fft_found = fft_found or (node == "fft")
+        visited.add(node)
 
-    for adj in graph[node]:
-        res += dfs(adj)
+        for adj in graph[node]:
+            res += dfs(adj, dac_found, fft_found)
 
-    visited.remove(node)
+        visited.remove(node)
+        cache[(node, dac_found, fft_found)] = res
 
-    return res
+    return cache[(node, dac_found, fft_found)]
 
-visited, graph = set(), dict()
+visited, graph, cache = set(), dict(), dict()
 
 while True:
     line = input()
@@ -30,6 +34,6 @@ while True:
     graph[key] = [s.strip() for s in lists.split(" ") if s]
 
 
-print(dfs("svr"))
+print(dfs("svr", False, False))
 
-# Unsolved!
+# Done!
